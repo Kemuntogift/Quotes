d83329b9278e9e96d4bf17a99a8fca7a38794251
Kemunto Gift <giftoyugi@gmail.com>
2022-04-10 12:57:40 +0300

    move quotes array to quote component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,5 @@ import { Quote } from './quote';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  quotes: Quote[] = [
-    {id: 1, excerpt:'Whoever said money cannot buy happiness simply did not know where to shop', author: 'Bo Derek', submitter: 'Gift Kemunto',},
-    { id: 2, excerpt: 'If you were born with the weakness to fall you were born with the strength to rise.', author: 'Rupi Kaur', submitter: 'Jane Doe' },
-    { id: 3, excerpt: 'Do not forget to drink water and get sun. You are  basically a houseplant with complicated emotions.', author: 'Anonymous', submitter: 'John Doe' },
-    { id: 4, excerpt: 'The best ideas come as jokes so make your thinking as funny as possible', author: 'David Oglivy', submitter: 'Miss Jagger' },
-  ];
+  
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote'; 
 
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css']
+  styleUrls: ['./quote.component.css'],
 })
 export class QuoteComponent implements OnInit {
+  quotes: Quote[] = [
+    {
+      id: 1,
+      excerpt:
+        'Whoever said money cannot buy happiness simply did not know where to shop',
+      author: 'Bo Derek',
+      submitter: 'Gift Kemunto',
+    },
+    {
+      id: 2,
+      excerpt:
+        'If you were born with the weakness to fall you were born with the strength to rise.',
+      author: 'Rupi Kaur',
+      submitter: 'Jane Doe',
+    },
+    {
+      id: 3,
+      excerpt:
+        'Do not forget to drink water and get sun. You are  basically a houseplant with complicated emotions.',
+      author: 'Anonymous',
+      submitter: 'John Doe',
+    },
+    {
+      id: 4,
+      excerpt:
+        'The best ideas come as jokes so make your thinking as funny as possible',
+      author: 'David Oglivy',
+      submitter: 'Miss Jagger',
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
