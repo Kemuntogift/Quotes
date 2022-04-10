ef2216c415db75d6f3fffe7c0cb38a470c6adc88
Kemunto Gift <giftoyugi@gmail.com>
2022-04-10 13:52:58 +0300

    add function for toggling details

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,11 +8,35 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1,'Whoever said money cannot buy happiness simply did not know where to shop','Bo Derek','Gift Kemunto'),
-    new Quote(2, 'If you were born with the weakness to fall you were born with the strength to rise.','Rupi Kaur','Jane Doe'),
-    new Quote(3,'Do not forget to drink water and get sun. You are  basically a houseplant with complicated emotions.','Anonymous','John Doe'),
-    new Quote(4,'The best ideas come as jokes so make your thinking as funny as possible','David Oglivy','Miss Jagger'),
+    new Quote(
+      1,
+      'Whoever said money cannot buy happiness simply did not know where to shop',
+      'Bo Derek',
+      'Gift Kemunto'
+    ),
+    new Quote(
+      2,
+      'If you were born with the weakness to fall you were born with the strength to rise.',
+      'Rupi Kaur',
+      'Jane Doe'
+    ),
+    new Quote(
+      3,
+      'Do not forget to drink water and get sun. You are  basically a houseplant with complicated emotions.',
+      'Anonymous',
+      'John Doe'
+    ),
+    new Quote(
+      4,
+      'The best ideas come as jokes so make your thinking as funny as possible',
+      'David Oglivy',
+      'Miss Jagger'
+    ),
   ];
+
+  toggleDetails(index: any) {
+    this.quotes[index].showDetails= !this.quotes[index].showDetails;
+  }
   constructor() {}
 
   ngOnInit(): void {}
