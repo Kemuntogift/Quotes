88547647113ec6ab9b78b5173b3222bc93626103
Kemunto Gift <giftoyugi@gmail.com>
2022-04-11 07:48:13 +0300

    add logic and function to get time passed in days

--- src/app/pass-days.pipe.ts
+++ src/app/pass-days.pipe.ts
@@ -1,12 +1,27 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'passDays'
+  name: 'passDays',
 })
 export class PassDaysPipe implements PipeTransform {
+  transform(value: any): number {
+    let today: Date = new Date(); //get current date and time
+    let todayWithNoTime: any = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate()
+    );
+    // var dateDifference = Math.abs(value - todayWithNoTime) //returns value in miliseconds
+    var dateDifference = Math.abs(todayWithNoTime - value);
+    const secondsInDay = 86400; //for all 24 hours
+    var dateDifferenceSeconds = dateDifference * 0.001; //converts miliseconds to seconds
+    var dateCounter = dateDifferenceSeconds / secondsInDay;
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    // if (dateCounter >= 1 && value > todayWithNoTime)
+    if (dateCounter >= 1 && value < todayWithNoTime) {
+      return dateCounter;
+    } else {
+      return 0;
+    }
   }
-
 }
